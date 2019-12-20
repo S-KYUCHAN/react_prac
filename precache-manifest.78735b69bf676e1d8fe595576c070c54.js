c1c4089f62683d2dffdf43e40ac7603db7d7fa3b
hawkeye <hawkeye@avengers.lab>
2019-12-19 23:10:57 -0800

    Updates

---
+++ precache-manifest.78735b69bf676e1d8fe595576c070c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1104c8a4b9298382e755cc3abdcfd9",
+    "url": "/react_prac/index.html"
+  },
+  {
+    "revision": "43f987dcb6a4b49bfee8",
+    "url": "/react_prac/static/css/main.22c47a16.chunk.css"
+  },
+  {
+    "revision": "da0f52e8893aa30c2982",
+    "url": "/react_prac/static/js/2.b2fb46e1.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react_prac/static/js/2.b2fb46e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43f987dcb6a4b49bfee8",
+    "url": "/react_prac/static/js/main.7adbae61.chunk.js"
+  },
+  {
+    "revision": "4441bb17d4fca083bd4a",
+    "url": "/react_prac/static/js/runtime-main.258667e3.js"
+  }
+]);
